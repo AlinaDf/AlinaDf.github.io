780aedc6a472214577cbd93344cf28db8dfd40cb
AlinaDf <114172190+AlinaDf@users.noreply.github.com>
2023-05-14 11:08:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,132 @@
+/* Slider */
+
+const prev = document.getElementById('btn-prev');
+const next = document.getElementById('btn-next');
+const slides = document.querySelectorAll('.intro__slider');
+const dots = document.querySelectorAll('.intro__dot');
+
+let index = 0;
+
+const activeSlide = n => {
+    for(slide of slides) {
+        slide.classList.remove('active');
+    }
+
+    slides[n].classList.add('active');
+}
+
+const activeDot = n => {
+    for(dot of dots) {
+        dot.classList.remove('active');
+    }
+
+    dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind => {
+    activeSlide(ind);
+    activeDot(ind);
+}
+
+const nextSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+}
+
+const prevSlide = () => {
+    if(index == 0) {
+        index = slides.length - 1
+        prepareCurrentSlide(index);
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+    }
+}
+
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+    });
+});
+
+const interval = setInterval(nextSlide, 2500);
+
+/* Маска номера телефона */
+
+let inputs = document.querySelectorAll('input[type="tel"]');
+let im = new Inputmask('+7 (999) 999-99-99');
+im.mask(inputs);
+
+/* Модальное окно */
+
+const modalBtn = document.querySelectorAll('[data-popup]');
+const modalClose = document.querySelectorAll('.modal__close');
+const modal = document.querySelectorAll('.popup');
+const modalBodyMob = document.querySelectorAll('.modal__mobile');
+const body = document.body;
+
+modalBtn.forEach(item => {
+    item.addEventListener('click', event => {
+        let $this = event.currentTarget;
+        let modalId = $this.getAttribute('data-popup');
+        let modal = document.getElementById(modalId);
+        let modalContent = modal.querySelector('.popup__dialog');
+
+        modalContent.addEventListener('click', event =>  {
+            event.stopPropagation();
+        });
+        console.log(modalId);
+
+        modal.classList.toggle('show');
+        $this.classList.toggle('active');
+        body.classList.toggle('no-scroll');
+
+        setTimeout(function() {
+            modalContent.style.transform = 'none';
+        }, 10)
+    });
+});
+
+modalClose.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.currentTarget.closest('.popup');
+
+        closeModal(currentModal);
+    });
+});
+
+modal.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.currentTarget;
+
+        closeModal(currentModal);
+    });
+});
+
+function closeModal(currentModal) {
+    let modalContent = currentModal.querySelector('.popup__dialog');
+    modalContent.removeAttribute('style');
+
+    setTimeout(function() {
+        currentModal.classList.remove('show');
+        body.classList.remove('no-scroll');
+    }, 100)
+} 
+
+/* Swiper */
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 6,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: true
+    },
+    speed: 15000,
+    simulateTouch: false,
+});
