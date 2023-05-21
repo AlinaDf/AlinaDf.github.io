6ee71a78eeee1e9813509665438682bf3b12c77e
AlinaDf <114172190+AlinaDf@users.noreply.github.com>
2023-05-21 13:27:47 +0300

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,44 @@
+/* Карта */
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [56.13825369257324,47.27149980396986],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 18
+    });
+
+    var myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point", // тип геометрии - точка
+            coordinates: [55.8, 37.8] // координаты точки
+        }
+    });
+
+    var placemark = new ymaps.Placemark([56.13825369257324,47.27149980396986], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/assets/img/map/map.svg',
+        iconImageSize: [100, 60],
+        iconImageOffset: [-20, -100]
+    });
+
+    myMap.geoObjects.add(myGeoObject); 
+    myMap.geoObjects.add(placemark); 
+
+    myMap.behaviors.disable('scrollZoom'); 
+
+    myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+    myMap.controls.remove('searchControl'); // удаляем поиск
+    myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+    myMap.controls.remove('typeSelector'); // удаляем тип
+    myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    myMap.controls.remove('rulerControl'); // удаляем контрол правил
+    myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+ymaps.ready(init)
--- js/script.js
+++ js/script.js
@@ -1,67 +1,59 @@
-/* Slider */
+// /* Slider */
 
-const prev = document.getElementById('btn-prev');
-const next = document.getElementById('btn-next');
-const slides = document.querySelectorAll('.intro__slider');
-const dots = document.querySelectorAll('.intro__dot');
+// const slides = document.querySelectorAll('.intro__slider');
+// const dots = document.querySelectorAll('.intro__dot');
 
-let index = 0;
+// let index = 0;
 
-const activeSlide = n => {
-    for(slide of slides) {
-        slide.classList.remove('active');
-    }
+// const activeSlide = n => {
+//     for(slide of slides) {
+//         slide.classList.remove('active');
+//     }
 
-    slides[n].classList.add('active');
-}
+//     slides[n].classList.add('active');
+// }
 
-const activeDot = n => {
-    for(dot of dots) {
-        dot.classList.remove('active');
-    }
+// const activeDot = n => {
+//     for(dot of dots) {
+//         dot.classList.remove('active');
+//     }
 
-    dots[n].classList.add('active');
-}
+//     dots[n].classList.add('active');
+// }
 
-const prepareCurrentSlide = ind => {
-    activeSlide(ind);
-    activeDot(ind);
-}
+// const prepareCurrentSlide = ind => {
+//     activeSlide(ind);
+//     activeDot(ind);
+// }
 
-const nextSlide = () => {
-    if(index == slides.length - 1) {
-        index = 0;
-        prepareCurrentSlide(index);
-    } else {
-        index++;
-        prepareCurrentSlide(index);
-    }
-}
+// const nextSlide = () => {
+//     if(index == slides.length - 1) {
+//         index = 0;
+//         prepareCurrentSlide(index);
+//     } else {
+//         index++;
+//         prepareCurrentSlide(index);
+//     }
+// }
 
-const prevSlide = () => {
-    if(index == 0) {
-        index = slides.length - 1
-        prepareCurrentSlide(index);
-    } else {
-        index--;
-        prepareCurrentSlide(index);
-    }
-}
+// const prevSlide = () => {
+//     if(index == 0) {
+//         index = slides.length - 1
+//         prepareCurrentSlide(index);
+//     } else {
+//         index--;
+//         prepareCurrentSlide(index);
+//     }
+// }
 
-dots.forEach((item, indexDot) => {
-    item.addEventListener('click', () => {
-        index = indexDot;
-        prepareCurrentSlide(index);
-    });
-});
+// dots.forEach((item, indexDot) => {
+//     item.addEventListener('click', () => {
+//         index = indexDot;
+//         prepareCurrentSlide(index);
+//     });
+// });
 
-const interval = setInterval(nextSlide, 2500);
-
-/* Маска номера телефона */
-
-let inputs = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask('+7 (999) 999-99-99');
-im.mask(inputs);
+// const interval = setInterval(nextSlide, 2500);
 
 /* Модальное окно */
 
@@ -121,12 +113,157 @@ function closeModal(currentModal) {
 
 /* Swiper */
 
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 6,
+document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth
+    if (width > 1024){
+        const swiper = new Swiper('.about__brands', {
+            loop: true,
+            Mode: true,
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+        
+            autoplay: {
+                delay: 5000,
+            },
+        });
+    }
+})
+
+new Swiper('.intro__body', {
+ // Optional parameters
+    loop: true,
+    Mode: true,
+
     autoplay: {
-        delay: 1,
-        disableOnInteraction: true
+        delay: 60000,
     },
-    speed: 15000,
-    simulateTouch: false,
+
+    pagination: {
+        el: '[data-slider-dots]',
+        type: "bullets",
+        clickable: true,
+        bulletClass: "dot",
+        bulletActiveClass: "dot_active",
+        renderBullet: function (index, className) {
+            return '<div class="' + className + '"></div>';
+        },
+    },
 });
+
+new Swiper('.news__slider', {
+    // Optional parameters
+    loop: true,
+    Mode: true,
+    slidesPerView: 3,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+document.querySelector('.news__slider').forEach(function(i) {
+    var Swiper = new Swiper (document.querySelector('.news__slider')[i], {
+        nextButton: document.querySelector('.swiper-button-prev')[i],
+        prevButton: document.querySelector('.swiper-button-next')[i],
+
+        slidesPerView: 2,
+    });
+});
+
+document.querySelector('.intro__body').forEach(function(i) {
+    var Swiper = new Swiper (document.querySelector('.intro__body')[i], {
+        swiperPagination: document.querySelector('.swiper-pagination')[i]
+    });
+});
+
+// jQuery(function($) {
+ 
+//   $('.swiper-object .swiper').each(function(index, value) {
+ 
+//     var mySwiper = new Swiper(value, {
+//       autoplay: {
+//         delay: 5000,
+//       },
+//       navigation: {
+//         nextEl: value.nextElementSibling.nextElementSibling,
+//         prevEl: value.nextElementSibling,
+//       },
+//       pagination: {
+//         el: value.nextElementSibling.nextElementSibling.nextElementSibling,
+//         clickable: true,
+//       },
+//       slidesPerView: 'auto',
+//     });
+ 
+//   });
+// });
+// const dom = {
+
+//     progress: {
+//         quiestionNumber: document.getElementById('quiestion-number'),
+//         totalQuestions: document.getElementById('total-questions')
+//     },
+
+//     quiz: {
+//         question: document.getElementById('quiestion'),
+//         questionPosition: document.getElementById('quiestion-position'),
+//     }
+// }
+
+/* Калькулятор */
+
+// const prev = document.getElementById('btn-prev');
+// const next = document.querySelectorAll('btn-next');
+// const quizes = document.querySelectorAll('.calc__content');
+// const values = document.querySelectorAll('.calc__item');
+
+// const activeQuize = n => {
+//     for(quize of quizes) {
+//         quize.classList.remove('active');
+//     }
+
+//     quizes[n].classList.add('active');
+// }
+
+// const activeValue = n => {
+//     for(value of values) {
+//         value.classList.remove('active');
+//     }
+
+//     values[n].classList.add('active');
+// }
+
+// const prepareCurrentQuize = ind => {
+//     activeQuize(ind);
+//     activeValue(ind);
+// }
+
+// const nextQuize = () => {
+//     if(index == quizes.length - 1) {
+//         index = 0;
+//         prepareCurrentQuize(index);
+//     } else {
+//         index++;
+//         prepareCurrentQuize(index);
+//     }
+// }
+
+// const prevQuize = () => {
+//     if(index == 0) {
+//         index = quizes.length - 1
+//         prepareCurrentQuize(index);
+//     } else {
+//         index--;
+//         prepareCurrentQuize(index);
+//     }
+// }
+
+// values.forEach((item, indexValue) => {
+//     item.addEventListener('click', () => {
+//         index = indexValue;
+//         prepareCurrentQuize(index);
+//     });
+// });
+
+// next.addEventListener('click', nextQuize);
